f1117ff185dc49b557920eebc7f9942794281e8b
student <student@240-02.INFORMATYKA.local>
2016-10-06 10:54:01 +0200

    zad2b

--- src/org/yourorghere/SimpleJOGL.java
+++ src/org/yourorghere/SimpleJOGL.java
@@ -97,10 +97,10 @@ public class SimpleJOGL implements GLEventListener {
         gl.glEnd();
         
         gl.glBegin(GL.GL_QUADS);
-        gl.glVertex3f( 1.0f,-1.0f, -6.0f);
-        gl.glVertex3f( 1.0f, 1.0f, -6.0f);
-        gl.glVertex3f( 2.0f, 1.0f, -6.0f);
-        gl.glVertex3f( 2.0f,-1.0f, -6.0f);
+        gl.glVertex3f( 1.0f,-1.0f, -9.0f);
+        gl.glVertex3f( 1.0f, 1.0f, -9.0f);
+        gl.glVertex3f( 2.0f, 1.0f, -9.0f);
+        gl.glVertex3f( 2.0f,-1.0f, -9.0f);
         gl.glEnd();
         
         // Flush all drawing operations to the graphics card
